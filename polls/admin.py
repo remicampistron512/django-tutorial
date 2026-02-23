95619b7bf34f63eb50730df5ce9dca485fb7a463
Rémi Campistron <remi.campistron@gmail.com>
2026-02-23 14:59:56 +0100

    Exercice d'administration - ex 4

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin  # type: ignore
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    list_filter = ('pub_date',)
+    ordering = ('pub_date',)
+    search_fields = ('question_text',)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text', 'votes')
+    list_filter = ('votes',)
+    ordering = ('votes',)
+    search_fields = ('choice_text',)
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+
