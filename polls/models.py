de2271906e9e12d70fc8a7fe02816ddc06a9e2d5
Rémi Campistron <remi.campistron@gmail.com>
2026-02-25 13:21:34 +0100

    5ème partie - Correction du bogue¶

--- polls/models.py
+++ polls/models.py
@@ -37,7 +37,8 @@ class Question(models.Model):
         return (max_choice.choice_text, max_choice.votes / total)
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def age(self):
         return timezone.now() - self.pub_date
