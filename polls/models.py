92d2640f8e867936d2355dea87f58968c4a44705
Rémi Campistron <remi.campistron@gmail.com>
2026-02-25 08:31:05 +0100

    exercice 2.2.3 2 (correction)

--- polls/models.py
+++ polls/models.py
@@ -2,13 +2,21 @@ import datetime
 from django.db import models  # type: ignore
 from django.utils import timezone  # type: ignore
 
+MAX_LENGTH = 20
+
+def text_excerpt(text, max_length):
+    return text[:max_length] + ('...' if len(text) > max_length
+                                else '')
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
-        return self.question_text[:20]
+        return text_excerpt(self.question_text, MAX_LENGTH)
+
+
+
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -16,10 +24,11 @@ class Question(models.Model):
     def age(self):
         return timezone.now() - self.pub_date
 
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text[:20]
+        return text_excerpt(self.choice_text, MAX_LENGTH)
