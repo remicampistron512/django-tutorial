f201fe80b9b72fdad3e6b61b1cb58ef7556980a8
Rémi Campistron <remi.campistron@gmail.com>
2026-02-26 14:18:54 +0100

    Tutoriel Partie 7 - Ajout d’objets liés

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin  # type: ignore
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date')
     list_filter = ('pub_date',)
@@ -9,8 +13,11 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ('question_text',)
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
+
+
 
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ('question', 'choice_text', 'votes')
